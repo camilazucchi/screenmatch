a1d8681cf98cd4a833167b47d7d6cc3e36ca03b0
camilazci <camilazucchi@outlook.com>
2023-12-06 16:00:14 -0300

    feat: implement DataConverter class and lambda expressions

--- src/main/java/br/com/alura/screenmatch/main/UserInteractionHandler.java
+++ src/main/java/br/com/alura/screenmatch/main/UserInteractionHandler.java
@@ -1,12 +1,22 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.model.EpisodesData;
+import br.com.alura.screenmatch.model.SeasonsData;
+import br.com.alura.screenmatch.model.SeriesData;
 import br.com.alura.screenmatch.service.ApiConsumer;
+import br.com.alura.screenmatch.service.DataConverter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInteractionHandler {
-    private final Scanner scanner = new Scanner(System.in);
-    private final ApiConsumer apiConsumer = new ApiConsumer();
+    private Scanner scanner = new Scanner(System.in);
+    private ApiConsumer apiConsumer = new ApiConsumer();
+    private DataConverter dataConverter = new DataConverter();
+
+    // Temporadas:
+    private List<SeasonsData> seasonsDataList = new ArrayList<>();
     private final String ADDRESS = "https://www.omdbapi.com/?t=";
     private final String APIKEY = "&apikey=35a0af28";
 
@@ -17,6 +27,24 @@ public class UserInteractionHandler {
         System.out.println("Digite o nome da série para busca: ");
         // Recebe o nome da série na variável "serieName" como uma String ("scanner.nextLine()"):
         var serieName = scanner.nextLine();
+        // Concatena o endereço:
         var json = apiConsumer.fetchData(ADDRESS + serieName.replace(" ", "+") + APIKEY);
+        // Conversor:
+        SeriesData seriesData = dataConverter.fetchData(json, SeriesData.class);
+        System.out.println(seriesData);
+
+        // Temporadas:
+        for (int i = 1; i <= seriesData.totalSeasons(); i++) {
+            json = apiConsumer.fetchData(ADDRESS + serieName.replace(" ", "+") + "&season="
+                    + i + APIKEY);
+            SeasonsData seasonsData = dataConverter.fetchData(json, SeasonsData.class);
+            seasonsDataList.add(seasonsData);
+        }
+        seasonsDataList.forEach(System.out::println);
+
+        /* Essa linha de código está usando expressões lambda e o método "forEach" para percorrer uma lista de objetos
+        * "SeasonData" e, para cada temporada, está percorrendo a lista de episódios associada a essa temporada
+        * ("s.episodes()"), imprimindo o títlo de cada episódio no console. */
+        seasonsDataList.forEach(s -> s.episodes().forEach(e -> System.out.println(e.title())));
     }
 }
