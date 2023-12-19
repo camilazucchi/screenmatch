d1086517101e19afc74b6ef1e4e4ad613b5ab5b8
camilazci <camilazucchi@outlook.com>
2023-12-19 16:53:35 -0300

    feat: implement episode search by season

--- src/main/java/br/com/alura/screenmatch/main/UserInteractionHandler.java
+++ src/main/java/br/com/alura/screenmatch/main/UserInteractionHandler.java
@@ -9,10 +9,7 @@ import br.com.alura.screenmatch.service.DataConverter;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UserInteractionHandler {
@@ -62,16 +59,22 @@ public class UserInteractionHandler {
                 /* "collect(Collectors.toList())" é diferente de "toList()"! */
                 //.collect(Collectors.toList());
 
-        System.out.println("\n0s 5 melhores episódios da série " + serieName.toUpperCase() + " são: ");
+        /* System.out.println("\n0s 10 melhores episódios da série " + serieName.toUpperCase() + " são: ");
         episodesDataList.stream()
                 // Filtra e limpa os episódios sem avaliação (com avaliação N/A):
                 .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A)" + e))
                 // Compara os episódios pela avaliação em ordem DECRESCENTE:
                 .sorted(Comparator.comparing(EpisodesData::rating).reversed())
+                .peek(e -> System.out.println("Ordenação: " + e))
                 // Limita o tamanho em 5:
-                .limit(5)
-                .forEach(System.out::println);
+                .limit(10)
+                .peek(e -> System.out.println("Limite: " + e))
+                .map(e -> e.title().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento: " + e))
+                .forEach(System.out::println); */
 
+        // Descobre as temporadas dos episódios:
         List<Episode> episodes = seasonsDataList.stream()
                 .flatMap(s -> s.episodes().stream()
                         .map(e -> new Episode(s.number(), e))
@@ -79,7 +82,21 @@ public class UserInteractionHandler {
 
         episodes.forEach(System.out::println);
 
-        System.out.println("A partir de que ano você deseja ver os episódios?");
+        System.out.println("Digite o nome do episódio que você deseja procurar: ");
+        var titleSnippet = scanner.nextLine();
+        Optional<Episode> foundEpisode = episodes.stream()
+                .filter(e -> e.getTitle().toLowerCase().contains(titleSnippet))
+                // "findFirst" sempre segue a mesma ordem!
+                .findFirst();
+
+        if (foundEpisode.isPresent()) {
+            System.out.println("Episódio encontrado: " + foundEpisode.get().getTitle());
+            System.out.println("Temporada: " + foundEpisode.get().getSeason());
+        } else {
+            System.out.println("Episódio não encontrado!");
+        }
+
+        /* System.out.println("A partir de que ano você deseja ver os episódios?");
         var year = scanner.nextInt();
         scanner.nextLine();
 
@@ -95,7 +112,7 @@ public class UserInteractionHandler {
                         "Temporada: " + e.getSeason()
                         + " Episódio: " + e.getTitle()
                         + " Data de lançamento: " + e.getReleaseDate().format(dtf)
-                ));
+                )); */
 
     }
 }
