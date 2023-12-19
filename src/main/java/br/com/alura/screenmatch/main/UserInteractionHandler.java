48bd8149fabe4ef81c728ec12b2b2224e8abd6db
camilazci <camilazucchi@outlook.com>
2023-12-19 13:27:53 -0300

    feat: implement episodes list by rating and year

--- src/main/java/br/com/alura/screenmatch/main/UserInteractionHandler.java
+++ src/main/java/br/com/alura/screenmatch/main/UserInteractionHandler.java
@@ -1,14 +1,19 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.model.Episode;
 import br.com.alura.screenmatch.model.EpisodesData;
 import br.com.alura.screenmatch.model.SeasonsData;
 import br.com.alura.screenmatch.model.SeriesData;
 import br.com.alura.screenmatch.service.ApiConsumer;
 import br.com.alura.screenmatch.service.DataConverter;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UserInteractionHandler {
     private Scanner scanner = new Scanner(System.in);
@@ -46,5 +51,51 @@ public class UserInteractionHandler {
         * "SeasonData" e, para cada temporada, está percorrendo a lista de episódios associada a essa temporada
         * ("s.episodes()"), imprimindo o títlo de cada episódio no console. */
         seasonsDataList.forEach(s -> s.episodes().forEach(e -> System.out.println(e.title())));
+
+        /* Esse trecho de código está usando a API de Stream do Java para processar uma lista de objetos "SeasonsData"
+        * e, a partir dela, criar uma lista combinada de todos os objetos "EpisodesData" contidos em cada temporada.
+        * Em resumo: esse trecho de código extrai todos os episódios de todas as temporadas presentes na lista
+        * "seasonsDataList" e os armazena em uma nova lista chamada "episodesDataList". */
+        List<EpisodesData> episodesDataList = seasonsDataList.stream()
+                .flatMap(s -> s.episodes().stream())
+                .collect(Collectors.toCollection(ArrayList::new));
+                /* "collect(Collectors.toList())" é diferente de "toList()"! */
+                //.collect(Collectors.toList());
+
+        System.out.println("\n0s 5 melhores episódios da série " + serieName.toUpperCase() + " são: ");
+        episodesDataList.stream()
+                // Filtra e limpa os episódios sem avaliação (com avaliação N/A):
+                .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
+                // Compara os episódios pela avaliação em ordem DECRESCENTE:
+                .sorted(Comparator.comparing(EpisodesData::rating).reversed())
+                // Limita o tamanho em 5:
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episode> episodes = seasonsDataList.stream()
+                .flatMap(s -> s.episodes().stream()
+                        .map(e -> new Episode(s.number(), e))
+                ).collect(Collectors.toList());
+
+        episodes.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios?");
+        var year = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate localDate = LocalDate.of(year, 1, 1);
+
+        // Formatador de data:
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodes.stream()
+                // Diferente de null e posterior a data de busca:
+                .filter(e -> e.getReleaseDate() != null && e.getReleaseDate().isAfter(localDate))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getSeason()
+                        + " Episódio: " + e.getTitle()
+                        + " Data de lançamento: " + e.getReleaseDate().format(dtf)
+                ));
+
     }
 }
